5c85e9426984573aa62363a4306229cb04a8d778
Jonathan Reem <jonathan.reem@gmail.com>
2014-07-04 19:28:04 -0700

    (test) Added basic tests.

--- tests/index.js
+++ tests/index.js
@@ -1,8 +1,34 @@
 var connect = require('../lib');
 var demand = require('must');
+var shield = require('shield-func');
+var http = require('http');
 
 describe('n4-connect', function () {
   it('should exist', function () {
     demand(connect).to.exist();
+  });
+
+  describe('when called', function () {
+    it('should try to connect to localhost:7474', function (done) {
+      var server = http.createServer(shield(done)).listen(7474);
+      connect();
+      server.close();
+    });
+
+    describe('with a port number', function () {
+      it('should try to connect to that port', function (done) {
+        var server = http.createServer(shield(done)).listen(3456);
+        connect(3456);
+        server.close();
+      });
+    });
+
+    describe('with a url', function () {
+      it('should try to connect to that url', function (done) {
+        var server = http.createServer(shield(done)).listen(3000);
+        connect('127.0.0.1:3000');
+        server.close();
+      });
+    });
   });
 });
