d6c8aa2a45e4a6ddb034b75ab3001965dca1862c
Jonathan Reem <jonathan.reem@gmail.com>
2014-07-05 10:57:16 -0700

    (test) Scaffolded query testing.

--- tests/index.js
+++ tests/index.js
@@ -2,6 +2,12 @@ var connect = require('../lib');
 var demand = require('must');
 var http = require('http');
 
+function MockQuery(raw) {
+  this.raw = raw;
+}
+
+MockQuery.prototype.compile = function () { return this.raw; };
+
 describe('n4-connect', function () {
   it('should exist', function () {
     demand(connect).to.exist();
@@ -24,6 +30,29 @@ describe('n4-connect', function () {
       it('should try to connect to that url', function () {
         var connection = connect('127.0.0.1:3000');
         demand(connection.host).to.equal('127.0.0.1:3000');
+      });
+    });
+  });
+
+  describe('when connected', function () {
+    var spyrq, spyrs, server, connection;
+
+    before(function () {
+      server = http.createServer(function (req, res) {
+        spyrq = req;
+        spyrs = res;
+        res.end(200, "Received.");
+      }).listen(7474);
+      connection = connect();
+    });
+
+    after(function () {
+      server.close();
+    });
+
+    describe('and given a query as a string', function () {
+      it('should transmit that string to the database', function (done) {
+        done();
       });
     });
   });
