7e72abe68d4fb5f6134150b7f07f13913486a2af
Jonathan Reem <jonathan.reem@gmail.com>
2014-07-05 10:55:25 -0700

    (test) Makes tests check host instead of making requests.

--- tests/index.js
+++ tests/index.js
@@ -1,6 +1,5 @@
 var connect = require('../lib');
 var demand = require('must');
-var shield = require('shield-func');
 var http = require('http');
 
 describe('n4-connect', function () {
@@ -9,26 +8,24 @@ describe('n4-connect', function () {
   });
 
   describe('when called', function () {
-    it('should try to connect to localhost:7474', function (done) {
-      var server = http.createServer(shield(done)).listen(7474);
-      connect();
-      server.close();
+    it('should try to connect to localhost:7474', function () {
+      var connection = connect();
+      demand(connection.host).to.equal('127.0.0.1:7474');
     });
 
     describe('with a port number', function () {
-      it('should try to connect to that port', function (done) {
-        var server = http.createServer(shield(done)).listen(3456);
-        connect(3456);
-        server.close();
+      it('should try to connect to that port', function () {
+        var connection = connect(3456);
+        demand(connection.host).to.equal('127.0.0.1:3456');
       });
     });
 
     describe('with a url', function () {
-      it('should try to connect to that url', function (done) {
-        var server = http.createServer(shield(done)).listen(3000);
-        connect('127.0.0.1:3000');
-        server.close();
+      it('should try to connect to that url', function () {
+        var connection = connect('127.0.0.1:3000');
+        demand(connection.host).to.equal('127.0.0.1:3000');
       });
     });
   });
 });
+
